f0b54f511454ccb429682b2e872431e1df046402
mhegglinmichael <mhegglinmichael@gmail.com>
2020-06-12 11:21:53 -0700

    hello

---
+++ A6/hello.c
@@ -0,0 +1,21 @@
+
+/**
+ *	Author: Michael Hegglin
+ *	Date: May 13, 2020
+ *	Description: This is hello world
+ **/
+
+#include <stdio.h>
+
+int main (int argc, char ** argv)
+{
+	printf("Hello, world!\n");
+	return 0;
+}
+
+
+
+
+
+
+
